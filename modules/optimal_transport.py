e9a23f9d87b952148b6cf22d6b139c4c485eadd2
Nabeela Khan <44938648+foxrei-n@users.noreply.github.com>
2026-05-15 22:30:17 +0900

    modules

---
+++ modules/optimal_transport.py
@@ -0,0 +1,83 @@
+import math
+import warnings
+from functools import partial
+from typing import Optional
+
+import numpy as np
+import ot as pot
+import torch
+
+
+class OTPlanSampler:
+
+    def __init__(
+        self,
+        method: str,
+        reg: float = 0.05,
+        reg_m: float = 1.0,
+        normalize_cost: bool = False,
+        warn: bool = True,
+    ) -> None:
+        if method == "exact":
+            self.ot_fn = pot.emd
+        self.reg = reg
+        self.reg_m = reg_m
+        self.normalize_cost = normalize_cost
+        self.warn = warn
+
+    def get_map(self, x0, x1):
+        a, b = pot.unif(x0.shape[0]), pot.unif(x1.shape[0])
+        if x0.dim() > 2:
+            x0 = x0.reshape(x0.shape[0], -1)
+        if x1.dim() > 2:
+            x1 = x1.reshape(x1.shape[0], -1)
+        x1 = x1.reshape(x1.shape[0], -1)
+        M = torch.cdist(x0, x1) ** 2
+        if self.normalize_cost:
+            M = M / M.max()  
+        p = self.ot_fn(a, b, M.detach().cpu().numpy())
+        if not np.all(np.isfinite(p)):
+            print("ERROR: p is not finite")
+            print("Cost mean, max", M.mean(), M.max())
+        if np.abs(p.sum()) < 1e-8:
+            if self.warn:
+                warnings.warn("Numerical errors in OT plan, reverting to uniform plan.")
+            p = np.ones_like(p) / p.size
+        return p
+
+    def sample_map(self, pi, batch_size, replace=True):
+        p = pi.flatten()
+        p = p / p.sum()
+        choices = np.random.choice(
+            pi.shape[0] * pi.shape[1], p=p, size=batch_size, replace=replace
+        )
+        return np.divmod(choices, pi.shape[1])
+        
+    def sample_plan_with_multi_labels(self, x0, x1, y0=None, y1=None, replace=True):
+        pi = self.get_map(x0, x1)
+        i, j = self.sample_map(pi, x0.shape[0], replace=replace)
+        return (
+            x0[i],
+            x1[j],
+            [y[i] for y in y0] if y0 is not None else None,
+            [y[j] for y in y1] if y1 is not None else None,
+        )
+
+    def sample_trajectory(self, X):
+        times = X.shape[1]
+        pis = []
+        for t in range(times - 1):
+            pis.append(self.get_map(X[:, t], X[:, t + 1]))
+
+        indices = [np.arange(X.shape[0])]
+        for pi in pis:
+            j = []
+            for i in indices[-1]:
+                j.append(np.random.choice(pi.shape[1], p=pi[i] / pi[i].sum()))
+            indices.append(np.array(j))
+
+        to_return = []
+        for t in range(times):
+            to_return.append(X[:, t][indices[t]])
+        to_return = np.stack(to_return, axis=1)
+        return to_return
